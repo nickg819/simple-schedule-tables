8f769ef4de103a09dbc16947fe374cdf79adb020
Nick Gassmann <nickgassmann@gmail.com>
2011-11-27 00:47:57 -0800

    Setting up plugin repo

---
+++ simple-schedule-tables-t24.php
@@ -0,0 +1,29 @@
+<?php
+	/*
+	Plugin Name: Simple Schedule Table
+	Plugin URI: http://www.ten24creative.com/plugins/simplescheduletable
+	Description: Plugin for building an easy to manage schedule table.
+	Author: Nick Gassmann
+	Version: 0.1
+	Author URI: http://www.ten24creative.com
+	License: GPL2
+	*/
+?>
+
+<?php
+/*  Copyright 2011  Nick Gassmann  (email : nick@ten24creative.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
